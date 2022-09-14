9fe36eca4e39aa12ee0a413273bfa23d286a5e29
Samir <imnotajedi@icloud.com>
2022-09-14 23:22:29 +0700

    Initial Commit

---
+++ AnimationHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationHW
+//
+//  Created by Самир Кафаров on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
